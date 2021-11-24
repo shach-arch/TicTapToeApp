135e19b56a9d652b2dfe2ffa0de737d7d37dc473
LynFurno64 <66757168+LynFurno64@users.noreply.github.com>
2021-11-23 22:04:58 -0400

    Start Screen and Enter Names Screen

--- app/src/main/java/com/example/tictactoeapp/MainActivity.java
+++ app/src/main/java/com/example/tictactoeapp/MainActivity.java
@@ -19,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
     public void playButtonClick(View view) {
         Intent intent = new Intent(this, PlayerChoiceActivity.class);
         startActivity(intent);
+
     }
 }
